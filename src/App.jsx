9bdb0a0b890b996e48e0d6974f06555420d4f5b9
Danny Mettler <danieljamespm@gmail.com>
2026-06-19 00:26:38 -0400

    created model for each workout and what will be the add workout feature.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import './App.css'
 
 function App() {
 
+  const [workouts, setWorkouts] = useState([])
+
+
+  function addWorkout() {
+    const newWorkout = {
+      id: Date.now(),
+      name,
+      sets: Number(sets),
+      reps: Number(reps),
+      completed: false
+    }
+    setWorkouts(prev => [...prev, newWorkout])
+  }
 
 
 }
